310e27354553551386e21f8bf77495299d3b0c27
Marcio <marciodegan@yahoo.com>
2022-01-22 20:39:46 -0300

    Exemplo de anotação @Autowired com parâmetro (required=false)

--- src/main/java/com/greatmeals/greatmealsapi/notificacao/NotificadorEmail.java
+++ src/main/java/com/greatmeals/greatmealsapi/notificacao/NotificadorEmail.java
@@ -1,26 +1,15 @@
 package com.greatmeals.greatmealsapi.notificacao;
 
 import com.greatmeals.greatmealsapi.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NotificadorEmail implements Notificador {
-
-    private boolean caixaAlta;
-    private String hostServidorSmtp;
-
-    public NotificadorEmail(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp;
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-
-        if (this.caixaAlta) {
-            mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificando %s através do e-mail %s usando STMP %s: %s\n ", cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
+        System.out.printf("Notificando %s através do e-mail %s: %s\n ", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
-    }
+
 }
--- src/main/java/com/greatmeals/greatmealsapi/service/AtivacaoClienteService.java
+++ src/main/java/com/greatmeals/greatmealsapi/service/AtivacaoClienteService.java
@@ -2,19 +2,22 @@ package com.greatmeals.greatmealsapi.service;
 
 import com.greatmeals.greatmealsapi.modelo.Cliente;
 import com.greatmeals.greatmealsapi.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
+    @Autowired(required = false)
     private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-        System.out.println("AtivacaoClienteService" + notificador);
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro foi ativado!");
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro foi ativado!");
+        } else {
+            System.out.println("Nao existe notificador mas cliente foi ativado");
+        }
     }
 }
