7ff284647f94087019a0d81d5218a12542008cf4
Marcio <marciodegan@yahoo.com>
2022-01-22 09:23:40 -0300

    project setup

---
+++ src/main/java/com/greatmeals/greatmealsapi/GreatmealsApiApplication.java
@@ -0,0 +1,13 @@
+package com.greatmeals.greatmealsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatmealsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatmealsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greatmeals/greatmealsapi/GreatmealsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatmeals.greatmealsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatmealsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
