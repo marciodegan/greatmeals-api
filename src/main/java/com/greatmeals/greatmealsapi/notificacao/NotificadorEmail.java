d36e703ac43a87f3094cf4d0ae7bf1cc631e8fd8
Marcio <marciodegan@yahoo.com>
2022-01-24 15:41:57 -0300

    exemplo de classe para NotificadorProperties

--- src/main/java/com/greatmeals/greatmealsapi/notificacao/NotificadorEmail.java
+++ src/main/java/com/greatmeals/greatmealsapi/notificacao/NotificadorEmail.java
@@ -1,7 +1,7 @@
 package com.greatmeals.greatmealsapi.notificacao;
 
 import com.greatmeals.greatmealsapi.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    private NotificadorProperties properties;
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta: " + properties.getPortaServidor());
 
         System.out.printf("Notificando %s através do e-mail %s: %s\n ", cliente.getNome(), cliente.getEmail(), mensagem);
     }
