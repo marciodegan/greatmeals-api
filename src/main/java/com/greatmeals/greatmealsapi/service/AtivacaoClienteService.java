718481f86f3315de87baa6181d1c1fc8062e260d
Marcio <marciodegan@yahoo.com>
2022-01-24 14:34:10 -0300

    Usando ApplicationEventPublisher - padrão Observer

--- src/main/java/com/greatmeals/greatmealsapi/service/AtivacaoClienteService.java
+++ src/main/java/com/greatmeals/greatmealsapi/service/AtivacaoClienteService.java
@@ -1,36 +1,20 @@
 package com.greatmeals.greatmealsapi.service;
 
 import com.greatmeals.greatmealsapi.modelo.Cliente;
-import com.greatmeals.greatmealsapi.notificacao.NivelUrgencia;
-import com.greatmeals.greatmealsapi.notificacao.Notificador;
-import com.greatmeals.greatmealsapi.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
-    private Notificador notificador;
+    private ApplicationEventPublisher eventPublisher;
 
-    @PostConstruct
-    public void init() {
-        System.out.println("INIT");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("Destroy");
-    }
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro foi ativado!");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 
     }
-
 }
---
+++ src/main/java/com/greatmeals/greatmealsapi/service/ClienteAtivadoEvent.java
@@ -0,0 +1,16 @@
+package com.greatmeals.greatmealsapi.service;
+
+import com.greatmeals.greatmealsapi.modelo.Cliente;
+
+public class ClienteAtivadoEvent {
+
+    private Cliente cliente;
+
+    public ClienteAtivadoEvent(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+}
