acec5b37fdf724820ebcd30f5c8ae1d3f43635d4
Marcio <marciodegan@yahoo.com>
2022-01-24 14:14:32 -0300

    Usando @Profile, @PostConstruct e @PreDestroy

--- src/main/java/com/greatmeals/greatmealsapi/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/greatmeals/greatmealsapi/notificacao/NotificadorEmailMock.java
@@ -4,19 +4,17 @@ import com.greatmeals.greatmealsapi.modelo.Cliente;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
-    public NotificadorEmail() {
-        System.out.println("Notificador email REAL");
+    public NotificadorEmailMock() {
+        System.out.println("NotificadorEmail MOCK");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s: %s\n ", cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("MOCK: Notificando %s através do e-mail %s: %s\n ", cliente.getNome(), cliente.getEmail(), mensagem);
     }
-
-
 }
--- src/main/java/com/greatmeals/greatmealsapi/service/AtivacaoClienteService.java
+++ src/main/java/com/greatmeals/greatmealsapi/service/AtivacaoClienteService.java
@@ -7,6 +7,9 @@ import com.greatmeals.greatmealsapi.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class AtivacaoClienteService {
 
@@ -14,6 +17,15 @@ public class AtivacaoClienteService {
     @Autowired
     private Notificador notificador;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("INIT");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destroy");
+    }
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
