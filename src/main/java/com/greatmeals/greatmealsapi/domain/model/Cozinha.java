c30fd652304b88aae52a3ec970fb188988b9bacf
Marcio <marciodegan@yahoo.com>
2022-01-25 15:04:52 -0300

    definindo domain models iniciais e adicionado jpa e mysql datasource

---
+++ src/main/java/com/greatmeals/greatmealsapi/domain/model/Cozinha.java
@@ -0,0 +1,49 @@
+package com.greatmeals.greatmealsapi.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cozinha")
+public class Cozinha {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome_cozinha")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cozinha cozinha = (Cozinha) o;
+
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
