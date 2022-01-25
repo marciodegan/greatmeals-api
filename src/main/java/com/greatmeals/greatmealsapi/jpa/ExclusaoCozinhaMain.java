e3a314fa683509598c4befc8d638a4d5975e6dab
Marcio <marciodegan@yahoo.com>
2022-01-25 16:34:15 -0300

    added método para remoção e classe ExclusaoCozinha com método main para teste

---
+++ src/main/java/com/greatmeals/greatmealsapi/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,25 @@
+package com.greatmeals.greatmealsapi.jpa;
+
+import com.greatmeals.greatmealsapi.GreatmealsApiApplication;
+import com.greatmeals.greatmealsapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class ExclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(GreatmealsApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+
+        cadastroCozinha.remover(cozinha);
+
+
+    }
+}
