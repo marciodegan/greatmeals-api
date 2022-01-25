115b4927e4cb64ae0c6c7b07d08e99bf08d137fe
Marcio <marciodegan@yahoo.com>
2022-01-25 16:18:09 -0300

    added método buscar e classe main para buscar cozinha

---
+++ src/main/java/com/greatmeals/greatmealsapi/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,24 @@
+package com.greatmeals.greatmealsapi.jpa;
+
+import com.greatmeals.greatmealsapi.GreatmealsApiApplication;
+import com.greatmeals.greatmealsapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(GreatmealsApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+    }
+}
