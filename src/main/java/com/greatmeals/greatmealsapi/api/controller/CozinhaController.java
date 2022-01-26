f0db4f3789bb92ec7118315380912d19dadabc46
Marcio <marciodegan@yahoo.com>
2022-01-26 19:48:57 -0300

    criado novo controller para Estado e exemplo de produces xml ou json

---
+++ src/main/java/com/greatmeals/greatmealsapi/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.greatmeals.greatmealsapi.api.controller;
+
+import com.greatmeals.greatmealsapi.domain.model.Cozinha;
+import com.greatmeals.greatmealsapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.todas();
+    }
+}
