d276a5e0faf52c94876bffd01504608501643a5f
Marcio <marciodegan@yahoo.com>
2022-01-26 20:34:02 -0300

    exemplo de bean ResponseStatus

--- src/main/java/com/greatmeals/greatmealsapi/api/controller/CozinhaController.java
+++ src/main/java/com/greatmeals/greatmealsapi/api/controller/CozinhaController.java
@@ -4,11 +4,9 @@ import com.greatmeals.greatmealsapi.api.model.CozinhasXmlWrapper;
 import com.greatmeals.greatmealsapi.domain.model.Cozinha;
 import com.greatmeals.greatmealsapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,7 @@ public class CozinhaController {
         return new CozinhasXmlWrapper(cozinhaRepository.todas());
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cozinhaId}")
     public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
         return cozinhaRepository.porId(id);
