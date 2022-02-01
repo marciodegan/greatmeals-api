2713970e185c0061c37096d7fa6ef4a1601b7347
Marcio <marciodegan@yahoo.com>
2022-02-01 14:16:36 -0300

    exemplo de jpql usando like

--- src/main/java/com/greatmeals/greatmealsapi/infrastructure/CozinhaRepositoryImpl.java
+++ src/main/java/com/greatmeals/greatmealsapi/infrastructure/CozinhaRepositoryImpl.java
@@ -23,8 +23,8 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 
     @Override
     public List<Cozinha> consultarPorNome(String nome) {
-        return manager.createQuery("from Cozinha where nome = :nome", Cozinha.class)
-                .setParameter("nome", nome)
+        return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+                .setParameter("nome", "%" + nome + "%")
                 .getResultList();
     }
 
