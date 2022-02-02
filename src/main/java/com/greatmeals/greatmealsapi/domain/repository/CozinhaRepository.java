3c6aeec3b054861210d0eb5e4c11cb32f969b97c
Marcio <marciodegan@yahoo.com>
2022-02-02 09:06:44 -0300

    outro exemplo de nome de método

--- src/main/java/com/greatmeals/greatmealsapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/greatmeals/greatmealsapi/domain/repository/CozinhaRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-    List<Cozinha> nome(String nome);
+    List<Cozinha> findByNome(String nome);
 
 }
