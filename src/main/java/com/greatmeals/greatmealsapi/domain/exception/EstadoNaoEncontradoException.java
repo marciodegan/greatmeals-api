9329fcdb62b766b7526ba9312c21be37313f995f
Marcio <marciodegan@yahoo.com>
2022-03-04 17:13:48 -0300

    Lançando exceptions de granularidade fina para entidade - Estado

---
+++ src/main/java/com/greatmeals/greatmealsapi/domain/exception/EstadoNaoEncontradoException.java
@@ -0,0 +1,12 @@
+package com.greatmeals.greatmealsapi.domain.exception;
+
+public class EstadoNaoEncontradoException extends EntidadeNaoEncontradaException {
+
+    public EstadoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+
+    public EstadoNaoEncontradoException(Long estadoId) {
+        this(String.format("Estado não foi encontrado com o número %d", estadoId));
+    }
+}
--- src/main/java/com/greatmeals/greatmealsapi/domain/service/CadastroEstadoService.java
+++ src/main/java/com/greatmeals/greatmealsapi/domain/service/CadastroEstadoService.java
@@ -1,7 +1,7 @@
 package com.greatmeals.greatmealsapi.domain.service;
 
 import com.greatmeals.greatmealsapi.domain.exception.EntidadeEmUsoException;
-import com.greatmeals.greatmealsapi.domain.exception.EntidadeNaoEncontradaException;
+import com.greatmeals.greatmealsapi.domain.exception.EstadoNaoEncontradoException;
 import com.greatmeals.greatmealsapi.domain.model.Estado;
 import com.greatmeals.greatmealsapi.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +9,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class CadastroEstadoService {
-
-    public static final String MSG_ESTADO_NAO_ENCONTRADO
-            = "Estado não foi encontrado com o número %d";
 
     public static final String MSG_ESTADO_EM_USO
             = "Estado de código %d não pode ser removido, pois já está em uso";
@@ -31,7 +26,7 @@ public class CadastroEstadoService {
         try {
             estadoRepository.deleteById(estadoId);
         } catch (EmptyResultDataAccessException e) {
-            throw new EntidadeNaoEncontradaException(String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId));
+            throw new EstadoNaoEncontradoException(estadoId);
 
         } catch (DataIntegrityViolationException e) {
             throw new EntidadeEmUsoException(String.format(MSG_ESTADO_EM_USO, estadoId));
@@ -40,7 +35,6 @@ public class CadastroEstadoService {
 
     public Estado buscarOuFalhar(Long estadoId) {
         return estadoRepository.findById(estadoId)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException(
-                        String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
+                .orElseThrow(() -> new EstadoNaoEncontradoException(estadoId));
     }
 }
