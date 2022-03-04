bb65ec7a0b990bd239ac9d88b27a2543412dde89
Marcio <marciodegan@yahoo.com>
2022-03-04 18:01:13 -0300

    Retirada ResponseStatus das Exceptions

--- src/main/java/com/greatmeals/greatmealsapi/domain/exception/NegocioException.java
+++ src/main/java/com/greatmeals/greatmealsapi/domain/exception/NegocioException.java
@@ -1,8 +1,5 @@
 package com.greatmeals.greatmealsapi.domain.exception;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class NegocioException extends RuntimeException {
 
     public NegocioException(String mensagem) {
