82fc135714b0ebcb568dac918fa8f15775dfcc6d
Marcio <marciodegan@yahoo.com>
2022-03-04 11:30:35 -0300

    Refatorado classes Service e Controller das entidades -> Cidade, Estado e Restaurante

--- src/main/java/com/greatmeals/greatmealsapi/api/controller/RestauranteController.java
+++ src/main/java/com/greatmeals/greatmealsapi/api/controller/RestauranteController.java
@@ -1,172 +1,55 @@
 package com.greatmeals.greatmealsapi.api.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greatmeals.greatmealsapi.domain.exception.EntidadeEmUsoException;
-import com.greatmeals.greatmealsapi.domain.exception.EntidadeNaoEncontradaException;
-import com.greatmeals.greatmealsapi.domain.model.Cozinha;
 import com.greatmeals.greatmealsapi.domain.model.Restaurante;
 import com.greatmeals.greatmealsapi.domain.repository.RestauranteRepository;
-import com.greatmeals.greatmealsapi.domain.service.RestauranteService;
-import com.greatmeals.greatmealsapi.infrastructure.repository.spec.RestauranteSpecs;
+import com.greatmeals.greatmealsapi.domain.service.CadastroRestauranteService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.ReflectionUtils;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping(value = "/restaurantes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestauranteController {
 
     @Autowired
-    private RestauranteService restauranteService;
+    private CadastroRestauranteService restauranteService;
 
     @Autowired
     private RestauranteRepository restauranteRepository;
 
     @GetMapping
-    public List<Restaurante> listar() {
-        List<Restaurante> restaurantes = restauranteRepository.findAll();
-        return restaurantes;
-    }
-
-    @GetMapping("/por-nome-e-frete")
-    public List<Restaurante> restaurantePorNomeEFrete(String nome,
-                                                      BigDecimal taxaFreteInicial,
-                                                      BigDecimal taxaFreteFinal) {
-        return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
-    }
-
-    @GetMapping("/primeiro")
-    public Optional<Restaurante> restaurantePrimeiro() {
-        return restauranteRepository.buscarPrimeiro();
-    }
-
-    @GetMapping("/com-frete-gratis")
-    public List<Restaurante> restauranteComFreteGraris(String nome) {
-
-        return restauranteRepository.findComFreteGratis(nome);
-    }
-
-    @GetMapping("/por-taxa-frete")
-    public List<Restaurante> restaurantePorTaxaFrete(
-            BigDecimal taxaInicial, BigDecimal taxaFinal) {
-        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
-    }
-
-    @GetMapping("/por-nome-e-cozinha")
-    public List<Restaurante> restaurantePorNomeECozinha(
-            String nome, Long cozinhaId) {
-        return restauranteRepository.consultarPorNome(nome, cozinhaId);
-    }
-
-    @GetMapping("/primeiro-por-nome")
-    public Optional<Restaurante> restaurantePrimeiroPorNome(String nome) {
-        return restauranteRepository.findFirstRestauranteByNomeContaining(nome);
-    }
-
-    @GetMapping("/top-2-por-nome")
-    public List<Restaurante> restaurantesTop2PorNome(String nome) {
-        return restauranteRepository.findTop2ByNomeContaining(nome);
-    }
-
-    @GetMapping("/restaurante-exists")
-    public boolean restaurantesExists(String nome) {
-        return restauranteRepository.existsByNomeContaining(nome);
-    }
-
-    @GetMapping("/restaurante-count-cozinha")
-    public int restaurantesCountCozinha(Long cozinhaId) {
-        return restauranteRepository.countByCozinhaId(cozinhaId);
-    }
+    public List<Restaurante> listar() { return restauranteRepository.findAll(); }
 
     @GetMapping("/{restauranteId}")
-    public ResponseEntity<Restaurante> buscar(@PathVariable("restauranteId") Long id) {
-        Optional<Restaurante> restaurante = restauranteRepository.findById(id);
-
-        if (restaurante.isPresent()) {
-            return ResponseEntity.ok(restaurante.get());
-        }
-        return ResponseEntity.notFound().build();
+    public Restaurante buscar(@PathVariable Long restauranteId) {
+        return restauranteService.buscarOuFalhar(restauranteId);
     }
 
     @PostMapping
-    public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
-        try {
-            restaurante = restauranteService.salvar(restaurante);
-
-            return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
-
-        } catch (EntidadeNaoEncontradaException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    @ResponseStatus(HttpStatus.CREATED)
+    public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+        return restauranteService.salvar(restaurante);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Restaurante restaurante) {
-        try {
-            Optional<Restaurante> restauranteEncontrado = restauranteRepository.findById(id);
+    @PutMapping("/{restauranteId}")
+    public Restaurante atualizar(@PathVariable Long restauranteId,
+                                 @RequestBody Restaurante restaurante) {
+        Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
 
-            if(restauranteEncontrado.isPresent()) {
-                BeanUtils.copyProperties(restaurante, restauranteEncontrado.get(),
-                        "id", "formasPagamento", "endereco", "dataCadastro", "cozinha", "produtos");
-                return ResponseEntity.ok(restauranteService.salvar(restauranteEncontrado.get()));
-            }
-            return ResponseEntity.notFound().build();
+        BeanUtils.copyProperties(restaurante, restauranteAtual,
+                        "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 
-        } catch (EntidadeNaoEncontradaException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return restauranteService.salvar(restauranteAtual);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
-        try {
-            restauranteService.remover(id);
-            return ResponseEntity.noContent().build();
-
-        } catch (EntidadeNaoEncontradaException e) {
-            return ResponseEntity.notFound().build();
-
-        } catch (EntidadeEmUsoException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
+    @DeleteMapping("/{restauranteId}")
+    public void remover(@PathVariable Long restauranteId) {
+        restauranteService.excluir(restauranteId);
     }
-
-    @PatchMapping("/{restauranteId}")
-    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
-                                              @RequestBody Map<String, Object> campos) {
-
-        Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
-        if(restauranteAtual.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        merge(campos, restauranteAtual.get());
-        return atualizar(restauranteId, restauranteAtual.get());
-    }
-
-    private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
-
-        camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-            field.setAccessible(true);
-
-            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-//            System.out.println(nomePropriedade + " = " + valorPropriedade);
-            ReflectionUtils.setField(field, restauranteDestino, novoValor);
-        });
-    }
-
 }
