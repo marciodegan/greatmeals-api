1429c4a807808c1025ac6c406787f9dcfe24f9ea
Marcio <marciodegan@yahoo.com>
2022-03-10 17:34:30 -0300

    Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/com/greatmeals/greatmealsapi/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/greatmeals/greatmealsapi/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.greatmeals.greatmealsapi.core.modelmapper;
 
+import com.greatmeals.greatmealsapi.api.model.RestauranteModel;
+import com.greatmeals.greatmealsapi.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,10 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+        return modelMapper;
     }
 }
