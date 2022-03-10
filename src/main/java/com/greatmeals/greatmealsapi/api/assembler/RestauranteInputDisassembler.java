dfbc04dde01334beb1c201107db0219674436e57
Marcio <marciodegan@yahoo.com>
2022-03-10 16:02:53 -0300

    Adicionado ModelMapper

--- src/main/java/com/greatmeals/greatmealsapi/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/greatmeals/greatmealsapi/api/assembler/RestauranteInputDisassembler.java
@@ -3,21 +3,17 @@ package com.greatmeals.greatmealsapi.api.assembler;
 import com.greatmeals.greatmealsapi.api.model.input.RestauranteInput;
 import com.greatmeals.greatmealsapi.domain.model.Cozinha;
 import com.greatmeals.greatmealsapi.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisassembler {
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
 
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return new ModelMapper().map(restauranteInput, Restaurante.class);
     }
 }
