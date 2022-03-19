d0b1b5d0bea6beae6c4f4b8c6042ffbebfaa1b83
Marcio <marciodegan@yahoo.com>
2022-03-19 12:43:44 -0300

    Fazendo projeção de recursos com @JsonView do Jackson - forma dinâmica

--- src/main/java/com/greatmeals/greatmealsapi/api/controller/RestauranteController.java
+++ src/main/java/com/greatmeals/greatmealsapi/api/controller/RestauranteController.java
@@ -16,6 +16,7 @@ import com.greatmeals.greatmealsapi.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,21 +41,38 @@ public class RestauranteController {
     private RestauranteRepository restauranteRepository;
 
     @GetMapping
-    public List<RestauranteModel> listar() {
-        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
+    public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
+        List<Restaurante> restaurantes = restauranteRepository.findAll();
+        List<RestauranteModel> restauranteModel = restauranteModelAssembler.toCollectionModel(restaurantes);
+
+        MappingJacksonValue restaurantesWrapper = new MappingJacksonValue(restauranteModel);
+
+        restaurantesWrapper.setSerializationView(RestauranteView.Resumo.class);
+
+        if("apenas-nome".equals(projecao)) {
+            restaurantesWrapper.setSerializationView(RestauranteView.ApenasNome.class);
+        } else if ("completo".equals(projecao)) {
+            restaurantesWrapper.setSerializationView(null);
+        }
+        return restaurantesWrapper;
     }
 
-    @JsonView(RestauranteView.Resumo.class)
-    @GetMapping(params = "projecao=resumo")
-    public List<RestauranteModel> listarResumido() {
-        return listar();
-    }
-
-    @JsonView(RestauranteView.ApenasNome.class)
-    @GetMapping(params = "projecao=apenas-nome")
-    public List<RestauranteModel> listarApenasNomes() {
-        return listar();
-    }
+//    @GetMapping
+//    public List<RestauranteModel> listar() {
+//        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
+//    }
+//
+//    @JsonView(RestauranteView.Resumo.class)
+//    @GetMapping(params = "projecao=resumo")
+//    public List<RestauranteModel> listarResumido() {
+//        return listar();
+//    }
+//
+//    @JsonView(RestauranteView.ApenasNome.class)
+//    @GetMapping(params = "projecao=apenas-nome")
+//    public List<RestauranteModel> listarApenasNomes() {
+//        return listar();
+//    }
 
     @GetMapping("/{restauranteId}")
     public RestauranteModel buscar(@PathVariable Long restauranteId) {
