d4c1e5ab4d55b4643f0c56075e942773742fbecb
Marcio <marciodegan@yahoo.com>
2022-03-23 17:08:45 -0300

    Criando bean de propriedades de configuração dos serviços de storage

--- src/main/java/com/greatmeals/greatmealsapi/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/greatmeals/greatmealsapi/infrastructure/service/storage/LocalFotoStorageService.java
@@ -1,7 +1,8 @@
 package com.greatmeals.greatmealsapi.infrastructure.service.storage;
 
+import com.greatmeals.greatmealsapi.core.storage.StorageProperties;
 import com.greatmeals.greatmealsapi.domain.service.FotoStorageService;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
@@ -12,8 +13,8 @@ import java.nio.file.Path;
 @Service
 public class LocalFotoStorageService implements FotoStorageService {
 
-    @Value("${greatmeals.storage.local.diretorio-fotos}")
-    private Path diretorioFotos;
+    @Autowired
+    private StorageProperties storageProperties;
 
     @Override
     public InputStream recuperar(String nomeArquivo) {
@@ -46,6 +47,6 @@ public class LocalFotoStorageService implements FotoStorageService {
     }
 
     private Path getArquivoPath(String nomeArquivo) {
-        return diretorioFotos.resolve(Path.of(nomeArquivo));
+        return storageProperties.getLocal().getDiretorioFotos().resolve(Path.of(nomeArquivo));
     }
 }
