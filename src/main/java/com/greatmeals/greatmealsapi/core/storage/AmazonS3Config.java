f139a3cc9bfe6786231f8d9b644eab8b0286d8c1
Marcio <marciodegan@yahoo.com>
2022-03-24 16:59:31 -0300

    Selecionando a implementação do serviço de storage de fotos

--- src/main/java/com/greatmeals/greatmealsapi/core/storage/AmazonS3Config.java
+++ src/main/java/com/greatmeals/greatmealsapi/core/storage/AmazonS3Config.java
@@ -4,7 +4,11 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.greatmeals.greatmealsapi.domain.service.FotoStorageService;
+import com.greatmeals.greatmealsapi.infrastructure.service.storage.LocalFotoStorageService;
+import com.greatmeals.greatmealsapi.infrastructure.service.storage.S3FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +19,7 @@ public class AmazonS3Config {
     private StorageProperties storageProperties;
 
     @Bean
+    @ConditionalOnProperty(name = "greatmeals.storage.tipo", havingValue = "s3") //bean será criado somente se o valor da property for s3
     public AmazonS3 amazonS3() {
         var credentials = new BasicAWSCredentials(
                 storageProperties.getS3().getIdChaveAcesso(),
@@ -24,5 +29,15 @@ public class AmazonS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(storageProperties.getS3().getRegiao())
                 .build();
+    }
+
+    @Bean // método que define o bean que retorna instancia do FotoStorageService
+    public FotoStorageService fotoStorageService() {
+        if(StorageProperties.TipoStorage.S3.equals(storageProperties.getTipo())) {
+            return new S3FotoStorageService();
+        } else {
+            return new LocalFotoStorageService();
+        }
+
     }
 }
--- src/main/java/com/greatmeals/greatmealsapi/core/storage/StorageProperties.java
+++ src/main/java/com/greatmeals/greatmealsapi/core/storage/StorageProperties.java
@@ -12,6 +12,19 @@ public class StorageProperties {
 
     private Local local = new Local();
     private S3 s3 = new S3();
+    private TipoStorage tipo = TipoStorage.LOCAL;
+
+    public TipoStorage getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoStorage tipo) {
+        this.tipo = tipo;
+    }
+
+    public enum TipoStorage {
+        LOCAL, S3
+    }
 
     public class Local {
         private Path diretorioFotos;
