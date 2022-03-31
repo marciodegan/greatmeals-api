ebea8682ba056c2dbf79f672e7b69274c3d6a8b8
Marcio <marciodegan@yahoo.com>
2022-03-31 11:35:02 -0300

    Descrevendo informações da API na documentação

--- src/main/java/com/greatmeals/greatmealsapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/greatmeals/greatmealsapi/core/openapi/SpringFoxConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,6 +28,16 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 //                        RequestHandlerSelectors.basePackage("com.greatmeals.greatmealsapi.outropacote")))
 //                .paths(PathSelectors.ant("/restaurantes/*"))
 //                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("GreatMeals API")
+                .description("Api aberta para clientes e restuarnates")
+                .version("1")
+                .contact(new Contact("GreatMeals", "https://www.greatmeals.com.br", "contato@greatmeals.com.br"))
                 .build();
     }
 
