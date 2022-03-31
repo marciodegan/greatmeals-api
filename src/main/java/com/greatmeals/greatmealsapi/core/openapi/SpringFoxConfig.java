350d89fb079db6804dfc9a86d441ecef67e83ae9
Marcio <marciodegan@yahoo.com>
2022-03-31 17:46:30 -0300

    Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/greatmeals/greatmealsapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/greatmeals/greatmealsapi/core/openapi/SpringFoxConfig.java
@@ -4,18 +4,25 @@ import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -33,8 +40,23 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 //                .paths(PathSelectors.ant("/restaurantes/*"))
 //                .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, globalGetResponseMessage())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
+    }
+
+    private List<ResponseMessage> globalGetResponseMessage() {
+        return Arrays.asList(
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .message("Erro interno do servidor")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.NOT_ACCEPTABLE.value())
+                        .message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+                        .build()
+        );
     }
 
     public ApiInfo apiInfo() {
